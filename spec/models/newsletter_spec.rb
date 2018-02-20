c235746a94e8c1943fa0cff67956f0fdbb064f4f
María Checa <MariaCheca@users.noreply.github.com>
2018-02-20 23:25:31 +0100

    Improved tests

--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -31,4 +31,18 @@ describe Newsletter do
     newsletter.from = "this_is_not_an_email"
     expect(newsletter).not_to be_valid
   end
+
+  describe '#list_of_recipients' do
+    it 'returns users segment list with newsletter enabled' do
+      user_with_newsletter_enabled = create(:user, newsletter: true)
+      user_with_newsletter_disabled = create(:user, newsletter: false)
+
+      newsletter.update(segment_recipient: 1)
+
+      users_list = newsletter.list_of_recipients
+
+      expect(users_list).to include user_with_newsletter_enabled
+      expect(users_list).not_to include user_with_newsletter_disabled
+    end
+  end
 end
