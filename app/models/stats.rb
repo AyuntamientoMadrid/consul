c08cf4c504f96d67abda73baf4de99ab9c02be44
Javi Martín <javim@elretirao.net>
2018-12-10 15:16:53 +0100

    Simplify the way we cache stats

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -6,6 +6,16 @@ class Stats
   end
 
   def generate
-    stats_methods.map { |stat_name| [stat_name, send(stat_name)] }.to_h
+    self.class.stats_methods.map { |stat_name| [stat_name, send(stat_name)] }.to_h
+  end
+
+  def self.stats_cache(*method_names)
+    method_names.each do |method_name|
+      alias_method :"raw_#{method_name}", method_name
+
+      define_method method_name do
+        stats_cache(method_name) { send(:"raw_#{method_name}") }
+      end
+    end
   end
 end
